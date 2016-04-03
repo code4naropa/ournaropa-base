57961e8b68a6859679265dc5a9774e5fc81a5d64
Finn Woelm <finn.woelm@gmail.com>
2016-04-03 06:59:39 -0600

    Trying to get images to work

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( ournaropa_library/*)
-Rails.application.config.assets.precompile += %w( ournaropa_calendar/*)
+#initializer :assets do |config|
+#  Rails.application.config.assets.paths << root.join("app", "assets", "images", #"ournaropa_library")
+#end
+#Rails.application.config.assets.precompile += %w( ournaropa_calendar/*)
