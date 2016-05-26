be246405a96430331e778153e6a9eb1b3dffebb1
Finn Woelm <finn.woelm@gmail.com>
2016-05-26 17:43:14 +0200

    Trying to serve email assets correctly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'www.ournaropa.org' }
-  config.action_mailer.asset_host = "localhost:3000"
+  config.action_mailer.asset_host = "http://www.ournaropa.org"
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_SMTP_USERNAME"],
     :password => ENV["SENDGRID_SMTP_PASSWORD"],
