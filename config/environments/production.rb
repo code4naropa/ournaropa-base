4ea50de8b7d2f0e0119c79f17dc57bb4361b9e10
Finn Woelm <finn.woelm@gmail.com>
2016-05-27 18:42:26 +0200

    Changed from OurNaropa to The Cushion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   config.log_level = :debug
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'www.ournaropa.org', :protocol => 'http' }
+  config.action_mailer.default_url_options = { :host => 'www.thecushion.org', :protocol => 'http' }
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_SMTP_USERNAME"],
     :password => ENV["SENDGRID_SMTP_PASSWORD"],
